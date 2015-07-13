baef46befc8b32c73f4acb94fd3c77566f9e635b
DeVillager <tommi.makelainen@helsinki.fi>
2015-07-13 19:34:29 +0300

    ElokuvakirjastoOsa1

--- test/movie_list_test.js
+++ test/movie_list_test.js
@@ -1,51 +1,88 @@
-describe('Movie list', function(){
-	var controller, scope;
+describe('ListController', function () {
+    var controller, scope;
 
-	var FirebaseServiceMock;
+    var FirebaseServiceMock;
 
-  	beforeEach(function(){
-  		// Lisää moduulisi nimi tähän
-    	module('MyAwesomeModule');
+    beforeEach(function () {
+        // Lisää moduulisi nimi tähän
+        module('MovieApp');
 
-    	FirebaseServiceMock = (function(){
-			return {
-				// Toteuta FirebaseServicen mockatut metodit tähän
-			}
-		})();
+        FirebaseServiceMock = (function () {
+            var movies = [
+                {
+                    kuvaus: 'iewfoi',
+                    nimi: 'ihie',
+                    ohjaaja: 'euhiueh',
+                    vuosi: '2000',
+                },
+                {
+                    kuvaus: 'iewewegweg',
+                    nimi: 'hrehea',
+                    ohjaaja: 'euhiueh',
+                    vuosi: '2678',
+                },
+                {
+                    kuvaus: 'rtntntss',
+                    nimi: 'fdzdn',
+                    ohjaaja: 'luflfyuk',
+                    vuosi: '1999',
+                },
+            ];
+            return {
+                // Toteuta FirebaseServicen mockatut metodit tähän
+                addMovie: function (movie) {
+                    movies.push(movie);
+                },
+                getMovies: function () {
+                    return movies;
+                },               
+                removeMovie: function (movie) {
+                    movies.splice(movies.indexOf(movie), 1);
+                }
+            };
+        })();
 
-		// Lisää vakoilijat
-	    // spyOn(FirebaseServiceMock, 'jokuFunktio').and.callThrough();
+        // Lisää vakoilijat
+        // spyOn(FirebaseServiceMock, 'jokuFunktio').and.callThrough();
 
-    	// Injektoi toteuttamasi kontrolleri tähän
-	    inject(function($controller, $rootScope) {
-	      scope = $rootScope.$new();
-	      // Muista vaihtaa oikea kontrollerin nimi!
-	      controller = $controller('MyAwesomeController', {
-	        $scope: scope,
-	        FirebaseService: FirebaseServiceMock
-	      });
-	    });
-  	});
+        spyOn(FirebaseServiceMock, 'removeMovie').and.callThrough();
+        spyOn(FirebaseServiceMock, 'getMovies').and.callThrough();
 
-  	/*
-  	* Testaa alla esitettyjä toimintoja kontrollerissasi
-  	*/
+        // Injektoi toteuttamasi kontrolleri tähän
+        inject(function ($controller, $rootScope) {
+            scope = $rootScope.$new();
+            // Muista vaihtaa oikea kontrollerin nimi!
+            controller = $controller('ListController', {
+                $scope: scope,
+                FirebaseService: FirebaseServiceMock
+            });
+        });
+    });
 
-  	/*
-  	* Testaa, että Firebasesta (mockilta) saadut elokuvat löytyvät konrollerista
-  	* Testaa myös, että Firebasea käyttävästä palvelusta kutsutaan oikeaa funktiota,
-  	* käyttämällä toBeCalled-oletusta.
-  	*/ 
-	it('should list all movies from the Firebase', function(){
-		expect(true).toBe(false);
-	});
+    /*
+     * Testaa alla esitettyjä toimintoja kontrollerissasi
+     */
 
-	/* 
-	* Testaa, että elokuvan pystyy poistamaan Firebasesta.
-	* Testaa myös, että Firebasea käyttävästä palvelusta kutsutaan oikeaa funktiota,
-  	* käyttämällä toBeCalled-oletusta.
-	*/
-	it('should be able to remove a movie', function(){
-		expect(true).toBe(false);
-	});
+    /*
+     * Testaa, että Firebasesta (mockilta) saadut elokuvat löytyvät konrollerista
+     * Testaa myös, että Firebasea käyttävästä palvelusta kutsutaan oikeaa funktiota,
+     * käyttämällä toBeCalled-oletusta.
+     */
+    it('should list all movies from the Firebase', function () {
+        expect(scope.movies.length).toBe(3);
+        expect(FirebaseServiceMock.getMovies).toHaveBeenCalled();
+    });
+
+    /* 
+     * Testaa, että elokuvan pystyy poistamaan Firebasesta.
+     * Testaa myös, että Firebasea käyttävästä palvelusta kutsutaan oikeaa funktiota,
+     * käyttämällä toBeCalled-oletusta.
+     */
+    it('should be able to remove a movie', function () {
+//        var listanKoko = scope.movies.length;
+        scope.remove(scope.movies[0]);
+        scope.remove(scope.movies[0]);
+        expect(scope.movies.length).toBe(1);
+        expect(FirebaseServiceMock.removeMovie).toHaveBeenCalled();
+    });
 });
--- web/app/app.js
+++ web/app/app.js
@@ -1,1 +1,22 @@
 // Toteuta moduulisi tänne
+var MovieApp = angular.module('MovieApp', ['firebase', 'ngRoute']);
+
+MovieApp.config(function ($routeProvider) {
+    $routeProvider
+            .when('/', {
+                controller: 'ListController',
+                templateUrl: 'app/views/list.html'
+            })
+            .when('/movies', {
+                controller: 'ListController',
+                templateUrl: 'app/views/list.html'
+            })
+            .when('/movies/new', {
+                controller: 'AddController',
+                templateUrl: 'app/views/add.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    // Lisää reitit tänne
+});
