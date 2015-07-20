cd1407a315b4cee27bb105826a271193377dee61
DeVillager <tommi.makelainen@helsinki.fi>
2015-07-20 19:07:48 +0300

    joo

--- web/app/controllers/list_controller.js
+++ web/app/controllers/list_controller.js
@@ -24,62 +24,19 @@ MovieApp.controller('ListController', function ($scope, $routeParams, FirebaseSe
     }
 
     $scope.find = function () {
-//        document.getElementById(otsikko).display = 'block';
-        var lista = [];
-        $scope.imdb = [];
-        APIService.findMovie($scope.etsiNimi).success(function (movies) {
-//            alert(movies.Search[0].Title);
 
-//            $scope.imdb.forEach(function (movie) {
-//                movie.nimi = movie.Title;
-//                movie.ohjaaja = 'joku';
-//                movie.
-//            });
-//            lista = $scope.imdb;
-            if ($scope.etsiNimi !== '' && $scope.etsiVuosi !== '') {
-                lista = movies.Search;
-                lista.forEach(function (movie) {
-                    if (movie.Year === $scope.etsiVuosi) {
-//                alert(movie.Title);
-//                    $scope.imdb.remove(movie);
-                        $scope.imdb.push(movie);
-                    }
-                });
-            }
-//            else if ($scope.etsiNimi === '' && $scope.etsiVuosi !== '') {
-//                APIService.findMovie($scope.etsiVuosi).success(function (movies2) {
-//                    $scope.imdb = movies2.Search;
-//                });
-            else if ($scope.etsiNimi !== '' && $scope.etsiVuosi === '') {
+        $scope.imdb = [];
+        APIService.findMovie($scope.etsiNimi, $scope.etsiVuosi).success(function (movies) {
+
                 $scope.imdb = movies.Search;
-            }
 
             if ($scope.imdb.length) {
                 $scope.found = $scope.imdb.length;
             } else {
                 $scope.found = 0;
             }
-
-
         });
-//        $scope.imdb = lista;
-
-//        $scope.imdb = lista;
-//        var lista = [];
-//        APIService.findMovie($scope.etsiVuosi).success(function (movies) {
-//            $scope.imdb = movies.Search;
-//
-//        });
-
     };
-
-
-
-//    if (movies[$routeParams.movie.toLowerCase()]) {
-//        $scope.movie = movies[$routeParams.movie.toLowerCase()];
-//    } else {
-//        $scope.movie = null;
-//    }
 
 });
 
--- web/app/services/api_service.js
+++ web/app/services/api_service.js
@@ -1,6 +1,6 @@
 MovieApp.service('APIService', function($http){
-  this.findMovie = function(name){
-    return $http.get('http://www.omdbapi.com', { params: { s: name } });
+  this.findMovie = function(name, year){
+    return $http.get('http://www.omdbapi.com', { params: { s: name, y:year } });
   }
 });
 
